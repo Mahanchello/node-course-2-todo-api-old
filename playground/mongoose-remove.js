f183250c8c48711a23e78258071e9e8bb71f4dd7
serge <serge@yahoo.com>
2018-08-03 11:58:49 -0400

    Add PATCH /todos/:id

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -4,13 +4,13 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
-// Todo.findByIdAndRemove('5b58a6e31947da67c659121d').then((todo) => {
-// 	console.log(todo);
-// });
-
-Todo.findOneAndRemove({_id:'5b58a6e31947da67c659121d'}).then((todo) => {
+Todo.findByIdAndRemove('5b58a6e31947da67c659121d').then((todo) => {
 	console.log(todo);
 });
+
+// Todo.findOneAndRemove({_id:'5b58a6e31947da67c659121d'}).then((todo) => {
+// 	console.log(todo);
+// });
 
 // Todo.remove({}).then((result) => {
 // 	console.log(result);
